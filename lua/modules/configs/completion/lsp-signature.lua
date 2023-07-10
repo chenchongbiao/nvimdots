58cbedb6a847dc8ebac3b71cd19bd10c85c9a99e
groveer <groveer@qq.com>
2023-07-10 15:36:16 +0800

    chore(lsp): disable hint text for lsp-signature

--- lua/modules/configs/completion/lsp-signature.lua
+++ lua/modules/configs/completion/lsp-signature.lua
@@ -6,7 +6,7 @@ return function()
 		floating_window = true,
 		floating_window_above_cur_line = true,
 		hi_parameter = "Search",
-		hint_enable = true,
+		hint_enable = false,
 		transparency = nil, -- disabled by default, allow floating win transparent value 1~100
 		wrap = true,
 		zindex = 45, -- avoid overlap with nvim.cmp
